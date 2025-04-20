29c4d193c59550041580011270ae71d4c9eeaba6
aamirkhan75 <aamirsrose4ever@yahoo.com>
2025-04-20 07:14:37 -0700

    FirstCommit

---
+++ app.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+app.get('/', function (req, res) {
+    res.render('index.ejs' );
+});
+
+app.get('/Aamir', function (req, res) {
+    res.send("Hello Aamir");
+});
+
+app.listen(process.env.PORT, function ()  {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
+
+
