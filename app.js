32fed481deec5230b13eebfe8d35ee19e5e52d8f
Ayush-Projects <kayush479@gmail.com>
2025-04-21 06:29:42 +0530

    fix indentation

--- app.js
+++ app.js
@@ -1,48 +1,36 @@
-const dotenv = require('dotenv');
-
+// Set up the environment variables
+const dotenv = require("dotenv");
 dotenv.config();
 
-const { auth } = require('express-openid-connect');
-const config = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: process.env.secret,
-    baseURL: process.env.baseURL,
-    clientID: process.env.clientID,
-    issuerBaseURL:process.env.issuerBaseURL
-  };
-  // auth router attaches /login, /logout, and /callback routes to the baseURL
-
-
-
-
-const express = require('express');
+// Importing packages and modules
+const { auth } = require("express-openid-connect");
+const express = require("express");
 const app = express();
+
+// Setting up the configuration
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.secret,
+  baseURL: process.env.baseURL,
+  clientID: process.env.clientID,
+  issuerBaseURL: process.env.issuerBaseURL,
+};
+app.set("view engine", "ejs");
 app.use(auth(config));
-app.set('view engine', 'ejs');
-app.get('/', function (req, res) {
-    res.render('index.ejs' );
-    
+
+app.get("/", function (req, res) {
+  res.render("index.ejs");
 });
 
-
-
-app.get('/dashboard',function(req,res){
-    res.send("Hello welcome to Dashboard");
-
-
-
+app.get("/dashboard", function (req, res) {
+  res.send("Hello welcome to Dashboard");
 });
 
-
-
-
-app.get('/Aamir', function (req, res) {
-    res.send("Hello Aamir");
+app.get("/Aamir", function (req, res) {
+  res.send("Hello Aamir");
 });
 
-app.listen(process.env.PORT, function ()  {
-    console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(process.env.PORT, function () {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
-
-
