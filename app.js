7649699785fd557f7e85d7205c69962e44c99402
aamirkhan75 <aamirsrose4ever@yahoo.com>
2025-04-20 17:41:08 -0700

    add autho

--- app.js
+++ app.js
@@ -2,13 +2,40 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+const { auth } = require('express-openid-connect');
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.secret,
+    baseURL: process.env.baseURL,
+    clientID: process.env.clientID,
+    issuerBaseURL:process.env.issuerBaseURL
+  };
+  // auth router attaches /login, /logout, and /callback routes to the baseURL
+
+
+
+
 const express = require('express');
 const app = express();
+app.use(auth(config));
 app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
     res.render('index.ejs' );
     
 });
+
+
+
+app.get('/dashboard',function(req,res){
+    res.send("Hello welcome to Dashboard");
+
+
+
+});
+
+
+
 
 app.get('/Aamir', function (req, res) {
     res.send("Hello Aamir");
